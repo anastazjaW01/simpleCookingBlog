da792e459427185d253b7bae870b2f6bbef8cdd3
anastazjaW01 <anastazyjka.wierzba@gmail.com>
2023-12-01 22:09:42 +0100

    First Commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Home</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <h3>New project !</h3>
+    </body>
+</html>
