c4bede9aef66fdcbf2f3b143427d925c4db6ad3f
anastazjaW01 <anastazyjka.wierzba@gmail.com>
2023-12-16 01:31:39 +0100

    Creating error 404 page

---
+++ 404.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css" integrity="sha384-b6lVK+yci+bfDmaY1u0zE8YYJt0TZxLEAFyYSLHId4xoVvsrQu3INevFKo+Xir8e" crossorigin="anonymous">
+        <link rel="stylesheet" href="styles/404.css?v=<?php echo time(); ?>">
+    </head>
+    <body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <div class="container-fluid pt-5">
+        <div class="row mt-5 pt-4">
+            <div class="col-8 offset-2">
+                    <p class="number">4<img class="rotate" src="images/path13547.svg" width=100 height="100"/>4</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-6 offset-3">
+                <h2>PAGE NOT FOUND</h2>
+                <p class="text404">This site may have been deleted or never existed. The cause of the error can also stand on the server side. 
+                           <br>Try typing the page address again or go back to the home page and browse further.</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-8 offset-2">
+                <a href="homePage.php" class="btnHome"><button class="btn btn-success styleBtn"><small>GO TO HOMEPAGE</small></button></a>
+            </div>
+        </div>
+    </div>
+    </body>
+</html>
