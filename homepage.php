c9cba996912807dcba0c9043759781bffc81705f
anastazjaW01 <anastazyjka.wierzba@gmail.com>
2023-12-18 02:41:21 +0100

    Add home page and style

---
+++ homepage.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Home</title>
+        <meta charset="utf-8">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css" integrity="sha384-b6lVK+yci+bfDmaY1u0zE8YYJt0TZxLEAFyYSLHId4xoVvsrQu3INevFKo+Xir8e" crossorigin="anonymous">
+        <link rel="stylesheet" href="styles/homepage.css?v=<?php echo time(); ?>">
+    </head>
+    <body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <div class="container-fluid p-0">
+    <nav class="kolorek navbar navbar-expand-lg bg-body-tertiary p-0">
+    <a class="navbar-brand active" aria-current="page" href="#"><img class="img-fluid m-2"  width="40" height="40" src="images/path13547.svg">Home</a>
+    <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="#">XXXXXX</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">XXXXXX</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">XXXXXX</a>
+            </li>
+        </ul>
+    </div>  
+    </div>
+    </nav>
+
+
+
+
+    </div>
+
+    </body>
+</html>
