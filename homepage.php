e828959fb537fc671bde85eb2f0afe146c132253
anastazjaW01 <anastazyjka.wierzba@gmail.com>
2023-12-20 01:39:00 +0100

    Creating home page navbar

--- homepage.php
+++ homepage.php
@@ -6,12 +6,13 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css" integrity="sha384-b6lVK+yci+bfDmaY1u0zE8YYJt0TZxLEAFyYSLHId4xoVvsrQu3INevFKo+Xir8e" crossorigin="anonymous">
         <link rel="stylesheet" href="styles/homepage.css?v=<?php echo time(); ?>">
+        <link rel="Shortcut icon" href="images/pathwhite.svg">
     </head>
     <body>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
     <div class="container-fluid p-0">
     <nav class="kolorek navbar navbar-expand-lg bg-body-tertiary p-0">
-    <a class="navbar-brand active" aria-current="page" href="#"><img class="img-fluid m-2"  width="40" height="40" src="images/path13547.svg">Home</a>
+    <a class="navbar-brand" aria-current="page" href="#"><img class="img-fluid m-2 logo" width="40" height="40" src="images/path13547.svg"><img class="img-fluid " width="80" height="80" src="images/textGreen.svg"></a>
     <div class="container-fluid">
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -19,15 +20,29 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav">
             <li class="nav-item">
-                <a class="nav-link" href="#">XXXXXX</a>
+                <a class="nav-link active" href="#">Home</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="#">XXXXXX</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="#">XXXXXX</a>
+            </li>
+            <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            XXXXXXX</a>
+            <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
             </li>
         </ul>
+        <form class="d-flex input-group justify-content-end" role="search">
+        <input class="search form-control-sm border-0" type="search" placeholder="Search" aria-label="Search" aria-describedby="searchbtn">
+        <button class="btn btn-sm btn-success" type="submit" id="searchbtn"><i class="bi bi-search"></i></button>
+        </form>
     </div>  
     </div>
     </nav>
--- index.php
+++ index.php
@@ -6,6 +6,7 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css" integrity="sha384-b6lVK+yci+bfDmaY1u0zE8YYJt0TZxLEAFyYSLHId4xoVvsrQu3INevFKo+Xir8e" crossorigin="anonymous">
         <link rel="stylesheet" href="styles/style.css?v=<?php echo time(); ?>">
+        <link rel="Shortcut icon" href="images/pathwhite.svg">
     </head>
     <body>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
@@ -13,7 +14,7 @@
         <div class="row">
         <div class="login col-lg-4 offset-lg-4 col-md-6 offset-md-3 col-sm-8 offset-sm-2 ">
         <div class="row">
-        <img class="" src="images/path13547.svg" width=50 height="50"/>
+        <a href="homePage.php" class="d-flex justify-content-center"><img src="images/path13547.svg" width=50 height="50"/></a>
             <h3>Simple Cooking</h3> 
         </div>
         <form>
@@ -27,7 +28,7 @@
                 <label for="password">Password</label>
                 <div class="row mt-1">
                     <div class="col-6">
-                    <input class="form-check-input" name="is_featured" type="checkbox" value="1" id="flexCheckDefault" style="border: seagreen 1px solid" checked>
+                    <input class="form-check-input" name="is_featured" type="checkbox" id="flexCheckDefault" style="border: seagreen 1px solid" checked>
                 <label class="form-check-label checkBtn" for="flexCheckDefault">
                     Remember me
                 </label>
