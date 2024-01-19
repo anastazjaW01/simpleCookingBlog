faf81c2fff6dcbba02c8b949f8f50f3853790acf
anastazjaW01 <anastazyjka.wierzba@gmail.com>
2024-01-20 00:43:40 +0100

    Add and create CategoryPage & SearchPage

--- homepage.php
+++ homepage.php
@@ -10,7 +10,7 @@
     </head>
     <body>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-    <div class="container-fluid p-0">
+    <div class="container-fluid p-0 position-relative" style="min-height:100vw">
     <!--Navbar--> 
     <nav class="color-nav navbar navbar-expand-lg bg-body-tertiary p-0">
     <div class="container-fluid">
@@ -28,7 +28,7 @@
             </li>         
             <!--data-bs-auto-close="outside" -->
             <li class="nav-item dropdown login-link">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <a class="nav-link dropdown-toggle" href="category.php" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Category</a>
             <ul class="dropdown-menu">
             <li><a class="dropdown-item" href="#">Soups</a></li>
@@ -61,133 +61,98 @@
     </div>
     </nav>
     <!--Container-->
-    <div class="container-fluid" style="margin-bottom: 5%;">
-    <div class="row offset-2  d-flex justify-content-center mx-5 px-5">
+    <div class="container-fluid main-container" >
+    <div class="row d-flex justify-content-center mx-lg-4 mx-md-2 mx-sm-1 px-lg-4 px-md-2 px-sm-1">
     <h3 class="d-flex justify-content-center pt-4">Hello foodie!</h3>
 
-  <div class="card big-card border flex-row mb-5 mt-3 p-0">
+    <div class="card big-card border flex-row mb-5 mt-3 p-0">
       <div class="card-left"><img src="images/test1.jpg" class="card-img-top img-fluid img-big" alt="New"></div>
-    <div class="card-body card-right">
+      <div class="card-body card-right">
       <h4 class="card-title ct-bg"><a href="#">Card title</a></h4>
       <p class="card-subtitle cst-bg">Author | Category</p>
       <p class="card-text start-text"> This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
       This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
       </p>
       <p class="card-text"><small class="text-body-secondary">Last updated 2 days ago</small></p>
+      </div>
+      </div>
+    </div>
+
+  <div class="row">
+    <div class="col-lg-4 col-md-6 col-sm-12 mb-3">
+    <div class="card little-card">
+    <div class="img-card"><img src="images/test1.jpg" class="card-img-top" alt="Recipe photo"></div>
+    <div class="card-body">
+    <h5 class="card-title">Recipe title</h5>
+    <p class="card-subtitle">Author | Category</p>
+    <p class="card-subtitle"><small class="date">12.01.2024</small></p>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-success" style="width:70%;">Read more</a>
+    </div>
     </div>
   </div>
+
+  <div class="col-lg-4 col-md-6 col-sm-12  mb-3">
+    <div class="card little-card">
+    <div class="img-card"><img src="images/test1.jpg" class="card-img-top" alt="Recipe photo"></div>
+    <div class="card-body">
+    <h5 class="card-title">Recipe title</h5>
+    <p class="card-subtitle">Author | Category</p>
+    <p class="card-subtitle"><small class="date">12.01.2024</small></p>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-success" style="width:70%;">Read more</a>
+  </div>
+</div>
+</div>
+<div class="col-lg-4 col-md-6 col-sm-12 mb-3">
+    <div class="card little-card">
+  <div class="img-card"><img src="images/test1.jpg" class="card-img-top" alt="Recipe photo"></div>
+  <div class="card-body">
+    <h5 class="card-title">Recipe title</h5>
+    <p class="card-subtitle">Author | Category</p>
+    <p class="card-subtitle"><small class="date">12.01.2024</small></p>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-success" style="width:70%;">Read more</a>
+  </div>
+</div>
+</div>
+<div class="col-lg-4 col-md-6 col-sm-12 mb-3">
+    <div class="card little-card">
+  <div class="img-card"><img src="images/test1.jpg" class="card-img-top" alt="Recipe photo"></div>
+  <div class="card-body">
+    <h5 class="card-title">Recipe title</h5>
+    <p class="card-subtitle">Author | Category</p>
+    <p class="card-subtitle"><small class="date">12.01.2024</small></p>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-success" style="width:70%;">Read more</a>
+  </div>
+</div>
+</div>
+<div class="col-lg-4 col-md-6 col-sm-12 mb-3">
+    <div class="card little-card">
+  <div class="img-card"><img src="images/test1.jpg" class="card-img-top" alt="Recipe photo"></div>
+  <div class="card-body">
+    <h5 class="card-title">Recipe title</h5>
+    <p class="card-subtitle">Author | Category</p>
+    <p class="card-subtitle"><small class="date">12.01.2024</small></p>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-success" style="width:70%;">Read more</a>
+  </div>
+</div>
+</div>
+<div class="col-lg-4 col-md-6 col-sm-12 mb-3">
+    <div class="card little-card">
+  <div class="img-card"><img src="images/test1.jpg" class="card-img-top" alt="Recipe photo"></div>
+  <div class="card-body">
+    <h5 class="card-title">Recipe title</h5>
+    <p class="card-subtitle">Author | Category</p>
+    <p class="card-subtitle"><small class="date">12.01.2024</small></p>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-success" style="width:70%;">Read more</a>
+  </div>
+</div>
 </div>
 
-    <div class="row">
-    <div class="col-lg-4 col-md-6 col-sm-12 d-flex justify-content-center mb-3">
-    <div class="card little-card">
-  <div class="img-card"><img src="images/test1.jpg" class="card-img-top" alt="Recipe photo"></div>
-  <div class="card-body">
-    <h5 class="card-title">Recipe title</h5>
-    <p class="card-subtitle">Author | Category</p>
-    <p class="card-subtitle"><small class="date">12.01.2024</small></p>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-success" style="width:70%;">Read more</a>
-  </div>
-</div>
-</div>
-
-<div class="col-lg-4 col-md-6 col-sm-12 d-flex justify-content-center mb-3">
-    <div class="card little-card">
-  <div class="img-card"><img src="images/test1.jpg" class="card-img-top" alt="Recipe photo"></div>
-  <div class="card-body">
-    <h5 class="card-title">Recipe title</h5>
-    <p class="card-subtitle">Author | Category</p>
-    <p class="card-subtitle"><small class="date">12.01.2024</small></p>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-success" style="width:70%;">Read more</a>
-  </div>
-</div>
-</div>
-<div class="col-lg-4 col-md-6 col-sm-12 d-flex justify-content-center mb-3">
-    <div class="card little-card">
-  <div class="img-card"><img src="images/test1.jpg" class="card-img-top" alt="Recipe photo"></div>
-  <div class="card-body">
-    <h5 class="card-title">Recipe title</h5>
-    <p class="card-subtitle">Author | Category</p>
-    <p class="card-subtitle"><small class="date">12.01.2024</small></p>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-success" style="width:70%;">Read more</a>
-  </div>
-</div>
-</div>
-<div class="col-lg-4 col-md-6 col-sm-12 d-flex justify-content-center mb-3">
-    <div class="card little-card">
-  <div class="img-card"><img src="images/test1.jpg" class="card-img-top" alt="Recipe photo"></div>
-  <div class="card-body">
-    <h5 class="card-title">Recipe title</h5>
-    <p class="card-subtitle">Author | Category</p>
-    <p class="card-subtitle"><small class="date">12.01.2024</small></p>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-success" style="width:70%;">Read more</a>
-  </div>
-</div>
-</div>
-<div class="col-lg-4 col-md-6 col-sm-12 d-flex justify-content-center mb-3">
-    <div class="card little-card">
-  <div class="img-card"><img src="images/test1.jpg" class="card-img-top" alt="Recipe photo"></div>
-  <div class="card-body">
-    <h5 class="card-title">Recipe title</h5>
-    <p class="card-subtitle">Author | Category</p>
-    <p class="card-subtitle"><small class="date">12.01.2024</small></p>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-success" style="width:70%;">Read more</a>
-  </div>
-</div>
-</div>
-<div class="col-lg-4 col-md-6 col-sm-12 d-flex justify-content-center mb-3">
-    <div class="card little-card">
-  <div class="img-card"><img src="images/test1.jpg" class="card-img-top" alt="Recipe photo"></div>
-  <div class="card-body">
-    <h5 class="card-title">Recipe title</h5>
-    <p class="card-subtitle">Author | Category</p>
-    <p class="card-subtitle"><small class="date">12.01.2024</small></p>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-success" style="width:70%;">Read more</a>
-  </div>
-</div>
-</div>
-<div class="col-lg-4 col-md-6 col-sm-12 d-flex justify-content-center mb-3">
-    <div class="card little-card">
-  <div class="img-card"><img src="images/test1.jpg" class="card-img-top" alt="Recipe photo"></div>
-  <div class="card-body">
-    <h5 class="card-title">Recipe title</h5>
-    <p class="card-subtitle">Author | Category</p>
-    <p class="card-subtitle"><small class="date">12.01.2024</small></p>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-success" style="width:70%;">Read more</a>
-  </div>
-</div>
-</div>
-<div class="col-lg-4 col-md-6 col-sm-12 d-flex justify-content-center mb-3">
-    <div class="card little-card">
-  <div class="img-card"><img src="images/test1.jpg" class="card-img-top" alt="Recipe photo"></div>
-  <div class="card-body">
-    <h5 class="card-title">Recipe title</h5>
-    <p class="card-subtitle">Author | Category</p>
-    <p class="card-subtitle"><small class="date">12.01.2024</small></p>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-success" style="width:70%;">Read more</a>
-  </div>
-</div>
-</div>
-<div class="col-lg-4 col-md-6 col-sm-12 d-flex justify-content-center mb-3">
-    <div class="card little-card">
-  <div class="img-card"><img src="images/test1.jpg" class="card-img-top" alt="Recipe photo"></div>
-  <div class="card-body">
-    <h5 class="card-title">Recipe title</h5>
-    <p class="card-subtitle">Author | Category</p>
-    <p class="card-subtitle"><small class="date">12.01.2024</small></p>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-success" style="width:70%;">Read more</a>
-  </div>
-</div>
-</div>
 
     </div>
     </div>
--- search.php
+++ search.php
@@ -1,18 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <title>About</title>
         <meta charset="utf-8">
+        <title>Category</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css" integrity="sha384-b6lVK+yci+bfDmaY1u0zE8YYJt0TZxLEAFyYSLHId4xoVvsrQu3INevFKo+Xir8e" crossorigin="anonymous">
-        <link rel="stylesheet" href="styles/about.css?v=<?php echo time(); ?>">
+        <link rel="stylesheet" href="styles/search.css?v=<?php echo time(); ?>">
         <link rel="Shortcut icon" href="images/pathwhite.svg">
-        <script defer src="scripts/about.js?v=<?php echo time(); ?>"></script>
     </head>
     <body>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-    <div class="container-fluid p-0 position-relative" style="min-height: 100vh;">
-    <!--Navbar-->
+    <div class="container-fluid p-0 position-relative " style="min-height: 100vh;">
+        <!--Navbar--> 
     <nav class="color-nav navbar navbar-expand-lg bg-body-tertiary p-0">
     <div class="container-fluid">
     <a class="navbar-brand" aria-current="page" href="homePage.php"><img class="img-fluid m-2 logo" width="40" height="40" src="images/path13547.svg"><img class="img-fluid " width="80" height="80" src="images/textGreen.svg"></a>
@@ -25,8 +24,9 @@
                 <a class="nav-link login-link" href="homePage.php">Home</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link login-link active" href="about.php">About</a>
+                <a class="nav-link login-link" href="about.php">About</a>
             </li>         
+            <!--data-bs-auto-close="outside" -->
             <li class="nav-item dropdown login-link">
             <a class="nav-link dropdown-toggle" href="category.php" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Category</a>
@@ -59,88 +59,89 @@
         </form>
     </div>  
     </div>
-    </nav>  
-    <!--Container--> 
-    <div class="container-fluid  ps-0 pe-0" style="padding-bottom:10%;">
-    <div class="section">
+    </nav>
+
+        <!--Container-->
+<div class="container-fluid main-container"> 
+    <div class="row title-cat">Search Panel</div>
     <div class="row">
-        <p class="title-section ">Meet us!</p>
-        <div class="arrow">
-        <span></span>
-        <span></span>
-        <span></span>
-        </div>
+        <div class="col-lg-4 col-md-6 col-sm-12 mb-5">
+         <div class="card little-card">
+         <div class="img-card"><img src="images/basil2.jpg" class="card-img-top" alt="Recipe photo"></div>
+         <div class="card-body">
+         <h5 class="card-title">Recipe title</h5>
+         <p class="card-subtitle">Author | Category</p>
+         <p class="card-subtitle"><small class="date">12.01.2024</small></p>
+         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+         <a href="#" class="btn btn-success" style="width:70%;">Read more</a>
+         </div>
     </div>
-    </div>
-    <div class="section snp">
-    <div class="row reveal">
-    <div class="col-6 img-p">
-            <div class="img-container">
-            <img class="img-fluid" src="images/soup0.jpg" alt="pic1"></div>
-        </div>
-        <div class="col-6">
-            <p><h2>Welcome to <small>Simple</small><b>Cooking</b>!</h2>
-                At Simple Cooking, we believe that the heart of every home is its kitchen. 
-                Our culinary journey began with a passion for creating delicious and memorable meals 
-                that bring people together. Whether you're a seasoned chef or a kitchen novice, 
-                our blog is your go-to resource for inspiration, recipes, and all things food-related.</p>
-        </div>
-    </div></div>
-    <div class="section">
-    <div class="row reveal" >
-        <div class="col-6">
-        <p><h2>Who We Are?</h2>
-        We are a team of food enthusiasts dedicated to exploring the diverse
-         and exciting world of culinary delights. Our goal is to share our love 
-         for cooking and eating through mouthwatering recipes, helpful tips, 
-         and engaging stories. We understand that food is more than just fuel;
-          it's a cultural experience, a source of joy, and a way to express creativity.       
-        </p>
-        </div>
-        <div class="col-6 img-np">
-        <div class="img-container">
-        <img class="img-fluid" src="images/eatingFriends.jpg" alt="pic1"></div>
-        </div>
-    </div></div>
-    <div class="section snp">
-    <div class="row reveal">
-    <div class="col-6 img-p pb-5">
-        <div class="img-container">
-        <img class="img-fluid" src="images/cookingMan.jpg" alt="pic1"></div>
-        </div>
-        <div class="col-6">
-        <p><h2>What You'll Find Here?</h2>
-           <ul>
-            <li><b>Inspiring Recipes:</b> From quick and easy weekday dinners to impressive
-            weekend feasts, our recipes are designed to suit every taste and skill level.</li>
-            <li><b>Cooking Tips and Tricks:</b> Learn valuable kitchen hacks, cooking techniques,
-            and tips to elevate your culinary skills.</li>
-            <li><b>Food Stories:</b> Discover the stories behind the dishes, exploring the cultural
-            and historical significance of the meals we create.</li>
-            <li><b>Community:</b> Join our growing community of food lovers! Share your own culinary
-            adventures, ask questions, and connect with like-minded individuals who share your passion
-            for good food.</li>
-           </ul>     
-        </p>
-        </div>
-    </div></div>
-    <div class="section">
-    <div class="row reveal" >
-        <div class="col-6">
-        <p><h2>Why We Do It?</h2>
-        Food is a universal language that transcends borders and brings people together. 
-        We are on a mission to inspire and empower you to create memorable moments in
-        your kitchen, whether you're cooking for yourself, your family, or friends.
-        Through the art of cooking, we hope to add a dash of joy and a pinch of 
-        creativity to your daily life.</p>
-        </div>
-        <div class="col-6 img-np">
-        <div class="img-container">
-        <img class="img-fluid" src="images/green.jpg" alt="pic1"></div>
-        </div>
-    </div></div>
-    </div>
-    <!--Footer--> 
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-12 mb-5">
+            <div class="card little-card">
+  <div class="img-card"><img src="images/basil2.jpg" class="card-img-top" alt="Recipe photo"></div>
+  <div class="card-body">
+    <h5 class="card-title">Recipe title</h5>
+    <p class="card-subtitle">Author | Category</p>
+    <p class="card-subtitle"><small class="date">12.01.2024</small></p>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-success" style="width:70%;">Read more</a>
+  </div>
+</div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-12 mb-5">
+            <div class="card little-card">
+  <div class="img-card"><img src="images/basil2.jpg" class="card-img-top" alt="Recipe photo"></div>
+  <div class="card-body">
+    <h5 class="card-title">Recipe title</h5>
+    <p class="card-subtitle">Author | Category</p>
+    <p class="card-subtitle"><small class="date">12.01.2024</small></p>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-success" style="width:70%;">Read more</a>
+  </div>
+</div>
+            </div>
+
+            <div class="col-lg-4 col-md-6 col-sm-12 mb-5">
+            <div class="card little-card">
+  <div class="img-card"><img src="images/basil2.jpg" class="card-img-top" alt="Recipe photo"></div>
+  <div class="card-body">
+    <h5 class="card-title">Recipe title</h5>
+    <p class="card-subtitle">Author | Category</p>
+    <p class="card-subtitle"><small class="date">12.01.2024</small></p>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-success" style="width:70%;">Read more</a>
+  </div>
+</div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-12 mb-5">
+            <div class="card little-card">
+  <div class="img-card"><img src="images/basil2.jpg" class="card-img-top" alt="Recipe photo"></div>
+  <div class="card-body">
+    <h5 class="card-title">Recipe title</h5>
+    <p class="card-subtitle">Author | Category</p>
+    <p class="card-subtitle"><small class="date">12.01.2024</small></p>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-success" style="width:70%;">Read more</a>
+  </div>
+</div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-12 mb-5">
+            <div class="card little-card">
+  <div class="img-card"><img src="images/basil2.jpg" class="card-img-top" alt="Recipe photo"></div>
+  <div class="card-body">
+    <h5 class="card-title">Recipe title</h5>
+    <p class="card-subtitle">Author | Category</p>
+    <p class="card-subtitle"><small class="date">12.01.2024</small></p>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-success" style="width:70%;">Read more</a>
+  </div>
+</div>
+</div>
+</div>
+</div>
+
+        <!--Footer-->
     <footer class="footer">
         <div class="row mt-1" style="width: 100%;">
             <div class="col-lg-4 col-sm-12 footer-elem">
