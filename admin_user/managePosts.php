66dabb15b8e621d0638bfd6f806f86e825d5b57c
anastazjaW01 <anastazyjka.wierzba@gmail.com>
2024-02-18 21:33:17 +0100

    Adding a subpage for managing users and comments copied from "managePosts"

--- admin_user/managePosts.php
+++ admin_user/managePosts.php
@@ -2,7 +2,7 @@
 <html>
     <head lang="en">
         <meta charset="utf-8">
-        <title>Manage Post</title>
+        <title>Manage Posts</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css" integrity="sha384-b6lVK+yci+bfDmaY1u0zE8YYJt0TZxLEAFyYSLHId4xoVvsrQu3INevFKo+Xir8e" crossorigin="anonymous">
         <link rel="stylesheet" href="../styles/managepages.css?v=<?php echo time(); ?>">
