2899be7ec50f5b467401a862da50a7eea318a0d0
anastazjaW01 <anastazyjka.wierzba@gmail.com>
2024-02-18 20:33:03 +0100

    Changing file name

--- admin_user/managePosts.php
+++ admin_user/managePosts.php
@@ -5,7 +5,7 @@
         <title>Manage Post</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css" integrity="sha384-b6lVK+yci+bfDmaY1u0zE8YYJt0TZxLEAFyYSLHId4xoVvsrQu3INevFKo+Xir8e" crossorigin="anonymous">
-        <link rel="stylesheet" href="../styles/managepost.css?v=<?php echo time(); ?>">
+        <link rel="stylesheet" href="../styles/managepages.css?v=<?php echo time(); ?>">
         <link rel="Shortcut icon" href="../images/pathwhite.svg"> 
     </head>
     <body>
