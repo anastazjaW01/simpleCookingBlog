5a5e2fb6eac09091bd500e783a7fbed2aee72721
anastazjaW01 <anastazyjka.wierzba@gmail.com>
2024-02-19 17:20:17 +0100

    Add PHP configuration constants for session, root URL, and database

---
+++ admin_user/config/connect.php
@@ -0,0 +1,7 @@
+<?php
+session_start();
+const ROOT_URL = 'http://localhost/simpleCookingBlog/';
+const DB_HOST = 'localhost';
+const DB_USER = 'root';
+const DB_PASS = '';
+const DB_NAME = 'cookingBlog';
