4f6f644c6e349fb9513c3d5267332c58df5b6612
anastazjaW01 <anastazyjka.wierzba@gmail.com>
2024-02-23 00:17:28 +0100

    Adding alerts and login field to registration page

--- signUp.php
+++ signUp.php
@@ -27,39 +27,90 @@ unset($_SESSION['signUp_data']);
         <a href="index.php" class="d-flex justify-content-center"><img src="images/path13547.svg" width=50 height="50"/></a>
             <h3>Simple Cooking</h3> 
         </div>
-        <form>
-            <div class="form1 form-floating mt-2">
-                <input type="email" id="email" name="email" value=""  class="form-control border-success" placeholder="jan-kowalski@example.com" required>
-                <span id="infoButton"><i class="bi bi-info-circle tt" id="info" title="Example e-mail should look like example@gmail.com"></i></span>
-                <label for="email">Email</label>
+        <!-- show alert by php when validation error occurs -->
+        <?php if(isset($_SESSION['signUp'])):?>
+            <div>
+            <p class="alert alert-danger alert-dismissible fade show" role="alert">
+            <i class="bi bi-exclamation-circle"></i> 
+            <?=$_SESSION['signUp'];
+                unset($_SESSION['signUp']);?>
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </p>
             </div>
-            <div class="row wrongEmail mt-1">
+        <?php endif ?>
+        <form action="<?= $root ?>registration_logic.php" enctype="multipart/form-data" method="POST">
+            <div class="form1 form-floating mt-2">
+                <input type="email" id="email" name="email" value="<?= $email ?>"  class="form-control border-success" placeholder="jan-kowalski@example.com" required>
+                <label for="email">Email</label>
+                <span id="infoButton"><i class="bi bi-info-circle tt" id="info" title="Example e-mail should look like example@gmail.com"></i></span>
+            </div>
+            <!-- wrong email alert -->
+            <div class="row wrong-alert alert-hidden mt-1">
                 <p class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" fill="currentColor" class="bi bi-exclamation-circle mb-1" viewBox="0 0 16 16">
                 <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
                 <path d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0z"/>
-                </svg> Wrong e-mail address! Try again.</p>
+                </svg> Wrong e-mail address! Try again!</p>
+                </div>
+            <div class="form1 form-floating mt-2">
+                <input type="text" id="login" name="login" value="<?= $login ?>" onkeyup="disableButton()"   class="form-control border-success" placeholder="User01" >
+                <span id="infoButton"><i class="bi bi-info-circle tt" id="info" title="For example: user_01"></i></span>
+                <label for="login">Login</label>
+            </div>
+            <!-- empty login alert -->
+            <div class="row wrong-alert alert-hidden mt-1">
+                <p class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" fill="currentColor" class="bi bi-exclamation-circle mb-1" viewBox="0 0 16 16">
+                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+                <path d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0z"/>
+                </svg> Login can't be empty!</p>
                 </div>
             <div class=" form1 form-floating mt-2">
-                <input type="password" class="form-control border-success" id="password" name="password" placeholder="Hasło" required>
+                <input type="password" class="form-control border-success" id="password" name="password" value="<?= $password ?>" placeholder="Hasło" required>
                 <span id="eyeButton" onclick="showHide()"><i class="bi bi-eye-fill" id="icon" onclick="changeIcon()"></i></span>
                 <label for="password">Password</label>
                 <div  class="progress" id="passStr">
                     <div id="passwordStrength" class="progress-bar bg-danger" role="progressbar" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100" style="width: 0%"></div>
                 </div>
             </div>
-            <div class="row wrongEmail mt-1">
+            <!-- wrong password alerts -->
+            <div class="row wrong-alert alert-hidden mt-1">
                 <p class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" fill="currentColor" class="bi bi-exclamation-circle mb-1" viewBox="0 0 16 16">
                 <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
                 <path d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0z"/>
                 </svg> The password must be at least 8 characters long</p>
-                </div>
+            </div>
+            <div class="row wrong-alert alert-hidden mt-1">
+                <p class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" fill="currentColor" class="bi bi-exclamation-circle mb-1" viewBox="0 0 16 16">
+                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+                <path d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0z"/>
+                </svg> The password must contain uppercase and lowercase letters!</p>
+            </div>
+            <div class="row wrong-alert alert-hidden mt-1">
+                <p class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" fill="currentColor" class="bi bi-exclamation-circle mb-1" viewBox="0 0 16 16">
+                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+                <path d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0z"/>
+                </svg> The password must contain numbers!</p>
+            </div>
+            <div class="row wrong-alert alert-hidden mt-1">
+                <p class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" fill="currentColor" class="bi bi-exclamation-circle mb-1" viewBox="0 0 16 16">
+                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+                <path d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0z"/>
+                </svg> The password must contain special chars!</p>
+            </div>
+            <!-- end of wrong password alerts -->
             <div class="form1 form-floating mt-2 mb-2" >
-                <input spellcheck="false" id="cpassword" name="cpassword" value="" onkeyup="disableButton()"  type="password"  class="form-control border-success"  placeholder="Powtórz hasło" required>
+                <input spellcheck="false" id="cpassword" name="cpassword" value="<?= $cpassword ?>" onkeyup="disableButton()"  type="password"  class="form-control border-success"  placeholder="Repeat password" required>
                 <span id="eyeButton1" onclick="showHide1()"><i class="bi bi-eye-fill" id="icon1" onclick="changeIcon1()"></i></span>
                 <label for="cpassword">Re password</label>
-                <button type="submit" class="btn btn-success col-12 mt-2 mb-1" id="logowanie" name="submit">Sing up</button>
+                <!-- password don't match alert -->
+                <div class="row wrong-alert alert-hidden mt-1">
+                <p class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" fill="currentColor" class="bi bi-exclamation-circle mb-1" viewBox="0 0 16 16">
+                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+                <path d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0z"/>
+                </svg> Password doesn't match!</p>
+            </div>
+                <button type="submit" class="btn btn-success col-12 mt-2 mb-1" id="logowanie" name="submit">Sign up</button>
                 <p style="color: #0f5132; font-size: small">You have account?
-                <a class="link-success fw-bold" href="signIn.php" style="font-size: small" id="example"  data-bs-placement="bottom">Sing in</a></p>
+                <a class="link-success fw-bold" href="signIn.php" style="font-size: small" id="example"  data-bs-placement="bottom">Sign in</a></p>
             </div>
         </form>
         </div>
