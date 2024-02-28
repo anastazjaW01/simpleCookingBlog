a7c300d5fe2385ee42764dca71e96b3e43f58d6d
anastazjaW01 <anastazyjka.wierzba@gmail.com>
2024-02-28 21:20:57 +0100

    Introducing changes to the navbar.php files and preparing to connect a script for changing themes

--- parts/navbar.php
+++ parts/navbar.php
@@ -16,6 +16,7 @@ $addictional_category_query = "SELECT * FROM categories WHERE type=1";
 $addictional_category_result = mysqli_query($conn, $addictional_category_query);
 
 $script="";
+
 if($name=="homepage"){ 
     $style=$name;
     $active1="active";
@@ -51,15 +52,17 @@ elseif($name=="about"){
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css" integrity="sha384-b6lVK+yci+bfDmaY1u0zE8YYJt0TZxLEAFyYSLHId4xoVvsrQu3INevFKo+Xir8e" crossorigin="anonymous">
         <link rel="stylesheet" href="styles/navbar_footer.css?v=<?php echo time(); ?>">
         <link rel="stylesheet" href="styles/<?=$style?>.css?v=<?php echo time(); ?>">
+        <link rel="stylesheet" href="styles/dark_light_mode.css?v=<?php echo time(); ?>">
+        <script src="scripts/colorTheme.js?v=<?php echo time(); ?>"></script>
         <link rel="Shortcut icon" href="images/pathwhite.svg">
     </head>
-    <body>
+    <body id="theme-body">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
     <div class="container-fluid p-0 position-relative" style="min-height:100vw">
     <!--Navbar--> 
     <nav class="color-nav navbar navbar-expand-lg bg-body-tertiary p-0">
     <div class="container-fluid">
-    <a class="navbar-brand" aria-current="page" href="<?= $root ?>index.php"><img class="img-fluid m-2 logo" width="40" height="40" src="images/path13547.svg"><img class="img-fluid " width="80" height="80" src="images/textGreen.svg"></a>
+    <a class="navbar-brand" aria-current="page" href="<?= $root ?>index.php"><img class="img-fluid m-2 logo" id="logo" width="40" height="40" src="images/path13547.svg"><img class="img-fluid" id="logoText" width="80" height="80" src="images/textGreen.svg"></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
@@ -99,7 +102,7 @@ elseif($name=="about"){
                 <ul class="dropdown-menu sub-menu">
                     <li><a class="dropdown-item" href="<?= $root ?>admin_user/createPost.php"><i class="bi bi-plus-lg pe-2"></i>Add post</a></li>
                     <li><a class="dropdown-item" href="<?= $root ?>admin_user/managePosts.php"><i class="bi bi-person-gear pe-2"></i>Manage panel</a></li>
-                    <li><button class="dropdown-item"><i class="bi bi-moon-stars pe-2"></i>Dark mode</a></button></li>
+                    <li><button class="btn dropdown-item" id="toogle-btn" onclick="changeColorMode()"><i class="bi bi-moon-stars pe-2" ></i>Dark mode</button></li>
                     <li><hr class="dropdown-divider"></li>
                     <li><a class="dropdown-item" href="<?= $root ?>signOut.php"><i class="bi bi-box-arrow-right pe-2"></i>Sign out</a></li>
                 </ul>
@@ -112,7 +115,7 @@ elseif($name=="about"){
         </ul>
         <form class="d-flex input-group justify-content-end searchForm mt-2 mt-sm-2 mt-md-2" role="search" action="search.php">
         <input class="search form-control-sm border-0" type="search" placeholder="Search" aria-label="Search" aria-describedby="searchbtn">
-        <button class="btn btn-sm btn-success border-0" type="submit" id="searchbtn"><i class="bi bi-search"></i></button>
+        <button class="btn btn-sm btn-successNav border-0" type="submit" id="searchbtn"><i class="bi bi-search"></i></button>
         </form>
     </div>  
     </div>
