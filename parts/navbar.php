6348eca6e84c164906192e5b264a392d66ac2169
anastazjaW01 <anastazyjka.wierzba@gmail.com>
2024-02-29 15:34:16 +0100

    Adding a condition checking the color theme for the user to the parts/navbar.php file and implementing it

--- parts/navbar.php
+++ parts/navbar.php
@@ -15,6 +15,21 @@ $main_category_result = mysqli_query($conn, $main_category_query);
 $addictional_category_query = "SELECT * FROM categories WHERE type=1";
 $addictional_category_result = mysqli_query($conn, $addictional_category_query);
 
+//get color_mode from database
+
+$query_color_mode = "SELECT * FROM colormode WHERE user_id = $id";
+$result_color_mode = mysqli_query($conn, $query_color_mode); // Corrected variable name
+$theme = mysqli_fetch_assoc($result_color_mode); // Corrected variable name
+$color_mode = $theme['color_mode'];
+
+//conditions for color theme 
+
+$bodyThemeClass = ($color_mode == 0) ? '' : 'dark-theme';
+$logoTheme = ($color_mode == 0) ? 'path13547.svg' : 'pathWhite.svg';
+$logoTextTheme = ($color_mode == 0) ? 'textGreen.svg' : 'textWhite.svg';
+$dropdownThemeClass = ($color_mode == 0) ? '' : 'dropdown-menu-dark';
+$btnText = ($color_mode == 0) ? '<i class="bi bi-moon-stars pe-2"></i>Dark mode' : '<i class="bi bi-brightness-high pe-2"></i>Light mode';
+
 $script="";
 
 if($name=="homepage"){ 
@@ -56,13 +71,13 @@ elseif($name=="about"){
         <script src="scripts/colorTheme.js?v=<?php echo time(); ?>"></script>
         <link rel="Shortcut icon" href="images/pathwhite.svg">
     </head>
-    <body id="theme-body">
+    <body class="<?= $bodyThemeClass ?>">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
     <div class="container-fluid p-0 position-relative" style="min-height:100vw">
     <!--Navbar--> 
     <nav class="color-nav navbar navbar-expand-lg bg-body-tertiary p-0">
     <div class="container-fluid">
-    <a class="navbar-brand" aria-current="page" href="<?= $root ?>index.php"><img class="img-fluid m-2 logo" id="logo" width="40" height="40" src="images/path13547.svg"><img class="img-fluid" id="logoText" width="80" height="80" src="images/textGreen.svg"></a>
+    <a class="navbar-brand" aria-current="page" href="<?= $root ?>index.php"><img class="img-fluid m-2 logo" id="logo" width="40" height="40" src="images/<?= $logoTheme ?>"><img class="img-fluid" id="logoText" width="80" height="80" src="images/<?= $logoTextTheme ?>"></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
@@ -78,7 +93,7 @@ elseif($name=="about"){
             <li class="nav-item dropdown login-link">
             <a class="nav-link dropdown-toggle <?=$active2?>" href="<?= $root ?>category.php" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Category</a>
-            <ul class="dropdown-menu">
+            <ul class="dropdown-menu <?= $dropdownThemeClass ?>">
             <?php while ($main_category=mysqli_fetch_assoc($main_category_result)) : ?>
             <li><a class="dropdown-item" href="<?=$root?>category.php?id=<?=$main_category['id']?>"><?=$main_category['name']?></a></li>
             <?php endwhile; ?>
@@ -86,7 +101,7 @@ elseif($name=="about"){
             <li class="dropend block-menu">
                 <a class="dropdown-item dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                 Cuisines of the world</a>
-                <ul class="dropdown-menu sub-menu">
+                <ul class="dropdown-menu sub-menu <?= $dropdownThemeClass ?>">
                     <?php while($addictional_category = mysqli_fetch_assoc($addictional_category_result)) : ?>
                     <li><a class="dropdown-item" href="<?= $root ?>category.php?id=<?= $addictional_category['id'] ?>"><?= $addictional_category['name'] ?></a></li>
                     <?php endwhile; ?>
@@ -99,10 +114,11 @@ elseif($name=="about"){
                 <a class="nav-link dropdown-toggle " href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                 <i class="bi bi-person pe-1"></i><?php echo htmlspecialchars($login['login'])?>
                 </a>
-                <ul class="dropdown-menu sub-menu">
+                <ul class="dropdown-menu sub-menu <?= $dropdownThemeClass ?>">
                     <li><a class="dropdown-item" href="<?= $root ?>admin_user/createPost.php"><i class="bi bi-plus-lg pe-2"></i>Add post</a></li>
                     <li><a class="dropdown-item" href="<?= $root ?>admin_user/managePosts.php"><i class="bi bi-person-gear pe-2"></i>Manage panel</a></li>
-                    <li><button class="btn dropdown-item" id="toogle-btn" onclick="changeColorMode()"><i class="bi bi-moon-stars pe-2" ></i>Dark mode</button></li>
+                    <li><form method="POST" action="<?= $root ?>colorMode.php"><button class="dropdown-item" name="theme" type="submit" id="toogle-btn"><?= $btnText ?></button>
+                    </form></li>
                     <li><hr class="dropdown-divider"></li>
                     <li><a class="dropdown-item" href="<?= $root ?>signOut.php"><i class="bi bi-box-arrow-right pe-2"></i>Sign out</a></li>
                 </ul>
