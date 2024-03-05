63102bd30c5b77de04665a028a9e372a216dacf3
anastazjaW01 <anastazyjka.wierzba@gmail.com>
2024-03-06 00:18:17 +0100

    Adding a script responsible for displaying comments from the database on the manageComments.php subpage

--- admin_user/manageComments.php
+++ admin_user/manageComments.php
@@ -1,6 +1,9 @@
 <?php
 $name = "managecomments";
 require "parts/navbar.php";
+
+$comment_query = "SELECT * FROM comments";
+$comment_result = mysqli_query($conn, $comment_query);
 ?>
             <!--Container-->
             <div class="container-fluid main-container">
@@ -15,6 +18,7 @@ require "parts/navbar.php";
                     </div>
                 </div>
                 <div class="col-lg-8 col-md-12 col-sm-12 col-12  info-table ">
+                <?php if(mysqli_num_rows($comment_result) > 0) : ?>
                     <table class="table">
                         <thead class="table-light sticky-header">
                             <tr>
@@ -25,44 +29,31 @@ require "parts/navbar.php";
                             </tr>
                         </thead>
                         <tbody>
+                            <?php while($comment = mysqli_fetch_assoc($comment_result)) : ?>
+                                <?php
+                                    //get comment author
+                                    $comm_author = $comment['user_id'];
+                                    $author_query = "SELECT login FROM users WHERE id=$comm_author";
+                                    $author_result = mysqli_query($conn, $author_query);
+                                    $author = mysqli_fetch_assoc($author_result);
+                                    //get post title
+                                    $post_title = $comment['post_id'];
+                                    $post_query = "SELECT title FROM posts WHERE id=$post_title";
+                                    $post_result = mysqli_query($conn, $post_query);
+                                    $title = mysqli_fetch_assoc($post_result);
+                                ?>
                             <tr>
-                                <td scope="row">Spaghetti Bolonese</td>
-                                <td>This is very good pasta!</td>
-                                <td>Jane1204</td>
-                                <td><button class="btn btn-danger">Delete</button></td>
+                                <td scope="row"><?= $title['title'] ?></td>
+                                <td><?= substr($comment['comm_text'], 0, 100) ?></td>
+                                <td><?= $author['login'] ?></td>
+                                <td><button class="btn btn-danger" href="<?= $root ?>admin_user/deleteComm.php?id=<?= $comment['id'] ?>" type="submit">Delete</button></td>
                             </tr>
-                            <tr>
-                                <td scope="row">Spaghetti Bolonese</td>
-                                <td>Italian cuisine</td>
-                                <td>Jane1204</td>
-                                <td><button class="btn btn-danger">Delete</button></td>
-                            </tr>
-                            <tr>
-                                <td scope="row">Spaghetti Bolonese</td>
-                                <td>Italian cuisine</td>
-                                <td>Jane1204</td>
-                                <td><button class="btn btn-danger">Delete</button></td>
-                            </tr>
-                            <tr>
-                                <td scope="row">Spaghetti Bolonese</td>
-                                <td>Italian cuisine</td>
-                                <td>Jane1204</td>
-                                <td><button class="btn btn-danger">Delete</button></td>
-                            </tr>
-                            <tr>
-                                <td scope="row">Spaghetti Bolonese</td>
-                                <td>Italian cuisine</td>
-                                <td>Jane1204</td>
-                                <td><button class="btn btn-danger">Delete</button></td>
-                            </tr>
-                            <tr>
-                                <td scope="row">Spaghetti Bolonese</td>
-                                <td>Italian cuisine</td>
-                                <td>Jane1204</td>
-                                <td><button class="btn btn-danger">Delete</button></td>
-                            </tr>
+                            <?php endwhile; ?>
                         </tbody>
                     </table>
+                    <?php else : ?>
+                        <div class="alert alert-secondary d-flex align-items-center fw-bold" role="alert"><?= "No comments to show." ?></div>
+                    <?php endif; ?>    
                 </div>
             </div>
             </div>
