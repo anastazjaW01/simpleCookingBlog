dc8170f665d1d243650588c131ed0bdd59573cde
anastazjaW01 <anastazyjka.wierzba@gmail.com>
2024-03-08 02:21:52 +0100

    Added password reset validation logic in confirmCode_logic.php

---
+++ confirmCode_logic.php
@@ -0,0 +1,21 @@
+<?php 
+
+require 'config/connectSession.php';
+$connection = new mysqli($host, $user, $pass, $db_name);
+
+if(isset($_SESSION['submit'])){
+    $code = filter_var($_POST['code'], FILTER_SANITIZE_NUMBER_INT);
+    $token = $_SESSION['token'];
+
+    $stmt = $connection -> prepare("SELECT * FROM users WHERE token = ?");
+    $stmt -> bind_param("s", $code);
+    $stmt -> execute();
+    $result = $stmt -> get_result();
+
+    if($result -> num_rows > 0 && $code === $token){
+        header('Location: ' . $root . 'resetPassword.php');
+        exit();
+    }else{
+        $_SESSION['code'] = "Invalid code!";
+    }
+}
