9ac227c0246974f335473c39f655337f980d0d4b
anastazjaW01 <anastazyjka.wierzba@gmail.com>
2024-03-10 02:10:58 +0100

    Adding a condition displaying the number of likes in the search.php file

--- search.php
+++ search.php
@@ -54,8 +54,11 @@ if(isset($_GET['search']) && isset($_GET['submit'])){
             <p class="card-subtitle"><small class="date"><?= date("m.d.Y",strtotime($post['date_time'])) ?></small></p>
             <p class="card-text"><?= substr($post['recipe_text'], 0, 150) ?>...</p>
             <a href="<?= $root ?>singlePost.php?id=<?= $post['id'] ?>" class="btn btn-success" style="width:70%;">Read more</a>
-            <a class="like" href="<?= $root ?>addLikes_logic.php?id=<?= $post['id'] ?>"><i class="<?= $all_class ?>" style="font-size:large;">
-            <small><?= $post['likes'] ?></small></i></a>
+            <a class="like" href="<?= $root ?>addLikes_logic.php?id=<?= $post['id'] ?>">
+              <i class="<?= $all_class ?>" style="font-size:large;">
+                  <small><?= $post['likes'] > 0 ? $post['likes'] : '' ?></small>
+              </i>
+            </a>
          </div>
       </div>
     </div>  
